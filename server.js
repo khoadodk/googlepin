ab30b922136dda9c6592417a164f7cf78f164434
Khoa Do <khoado.dk@gmail.com>
2020-03-30 21:09:54 -0400

    Added user and pin model

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const { ApolloServer } = require("apollo-server");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("---MongoDB connected!---"))
+  .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
