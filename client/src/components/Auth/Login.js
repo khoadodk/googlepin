922d279d978d4a2698ab06d5f69ce871b0168c31
Khoa Do <khoado.dk@gmail.com>
2020-03-30 23:00:49 -0400

    Added context to client and some stylings

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GoogleLogin } from "react-google-login";
 import { GraphQLClient } from "graphql-request";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
-const ME_QUERY = `{
-  me {
-    _id
-    name
-    email
-    picture
-  }
-}`;
+import Context from "../../context";
+import { ME_QUERY } from "../../graphql/queries";
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
   const onSuccess = async googleUser => {
-    // Grab the Oauth token
-    const idToken = googleUser.getAuthResponse().id_token;
-    // Send it to the server
-    const client = new GraphQLClient("http://localhost:4000/graphql", {
-      headers: { authorization: idToken }
-    });
-    const data = await client.request(ME_QUERY);
-    console.log(data);
+    try {
+      // Grab the Oauth token
+      const idToken = googleUser.getAuthResponse().id_token;
+      // Send it to the server
+      const client = new GraphQLClient("http://localhost:4000/graphql", {
+        headers: { authorization: idToken }
+      });
+      const data = await client.request(ME_QUERY);
+      // console.log(data);
+      dispatch({ type: "LOGIN_USER", payload: data.me });
+    } catch (err) {
+      onFailure(err);
+    }
+  };
+
+  const onFailure = err => {
+    console.error("Error Logging in !", err);
   };
 
   return (
-    <GoogleLogin
-      clientId="659726176561-fsnlg5uq75kjl4f83nk597a01e77177l.apps.googleusercontent.com"
-      onSuccess={onSuccess}
-      isSignedIn={true}
-    />
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)" }}
+      >
+        Welcome to Google Pin
+      </Typography>
+      <GoogleLogin
+        clientId="659726176561-fsnlg5uq75kjl4f83nk597a01e77177l.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        theme="dark"
+      />
+    </div>
   );
 };
 
